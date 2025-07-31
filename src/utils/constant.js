16db840d97b3c9a3ea365a26709e882e4b9982c3
Jatin Munjal <munjal.jatin24@gmail.com>
2025-07-31 21:40:32 +0530

    utility directory added

---
+++ src/utils/constant.js
@@ -0,0 +1,8 @@
+export const CDN_LINK =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_LINK =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUGz0WshFYnlwlqP_fs3ErIppUvpuaYfTrZQ&s";
+
+export const IMAGE_SECTION =
+  "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto/portal/m/seo/App_download_banner.png";
